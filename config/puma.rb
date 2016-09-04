543a9537f33c3c3858decba17fa8c5b5ad3cef8b
ryonext <ryonext.s@gmail.com>
2016-09-04 12:02:07 +0900

    Fix puma pids path.

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ bind "unix://#{shared_dir}/sockets/puma.sock"
 # Logging
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{shared_dir}/sockets/puma.pid"
+state_path "#{shared_dir}/sockets/puma.state"
 activate_control_app
 plugin :tmp_restart
