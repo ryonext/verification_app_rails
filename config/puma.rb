40e9641ba3e42267baa1156c875c2ad371b3fcc2
ryonext <ryonext.s@gmail.com>
2016-09-04 17:35:51 +0900

    Fix settings for launching app

--- config/puma.rb
+++ config/puma.rb
@@ -2,13 +2,11 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://tmp/puma.sock"
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
 # Set master PID and state locations
-pidfile "#{shared_dir}/sockets/puma.pid"
-state_path "#{shared_dir}/sockets/puma.state"
+pidfile "/tmp/puma.pid"
+state_path "/tmp/puma.state"
 activate_control_app
 plugin :tmp_restart
