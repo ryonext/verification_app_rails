4dff510a7124c8e243fc9f77b7c625986c3dac23
ryonext <ryonext.s@gmail.com>
2016-09-04 17:47:01 +0900

    Fix bind setting.

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
-bind "unix://tmp/puma.sock"
+bind "unix:///tmp/puma.sock"
 # Logging
 stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
 # Set master PID and state locations
