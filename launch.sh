69f541420137626dfc844444afd0e824fdc02abc
ryonext <ryonext.s@gmail.com>
2016-09-10 14:33:09 +0900

    Run db migration.

--- launch.sh
+++ launch.sh
@@ -1,3 +1,4 @@
 # /bin/sh
 cd /var/www
+/home/ec2-user/.rbenv/bin/rbenv exec bundle exec rake db:migrate RAILS_ENV=production
 SECRET_KEY_BASE=hoge /home/ec2-user/.rbenv/bin/rbenv exec bundle exec puma -e production -d
