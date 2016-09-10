6c24eae9a113aaab8e1c60a4306c6e3321383dab
ryonext <ryonext.s@gmail.com>
2016-09-10 15:54:14 +0900

    Reload puma if process exists.

--- launch.sh
+++ launch.sh
@@ -1,4 +1,11 @@
 # /bin/sh
 cd /var/www
 /home/ec2-user/.rbenv/bin/rbenv exec bundle exec rake db:migrate RAILS_ENV=production
-SECRET_KEY_BASE=hoge /home/ec2-user/.rbenv/bin/rbenv exec bundle exec puma -e production -d
+count=`ps -ef | grep puma | grep -v grep | wc -l`
+if [ $count = 0 ]; then
+  # start
+  SECRET_KEY_BASE=hoge /home/ec2-user/.rbenv/bin/rbenv exec bundle exec puma -e production -d
+else
+  # reload
+  /home/ec2-user/.rbenv/bin/rbenv exec bundle exec pumactl restart
+fi
