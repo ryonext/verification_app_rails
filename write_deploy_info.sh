8056987fa5de1254e31b82eb349d0420aca3ab13
ryonext <ryonext.s@gmail.com>
2016-10-18 15:19:27 +0900

    Write deploy info.

---
+++ write_deploy_info.sh
@@ -0,0 +1,8 @@
+# /bin/sh
+cd /var/www
+echo $APPLICATION_NAME >> REVISION.txt
+echo $DEPLOYMENT_ID >> REVISION.txt
+echo $DEPLOYMENT_GROUP_NAME >> REVISION.txt
+echo $DEPLOYMENT_GROUP_ID >> REVISION.txt
+echo $LIFECYCLE_EVENT >> REVISION.txt
+
